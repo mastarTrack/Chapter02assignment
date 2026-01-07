dc2312fab3f8b5ded7ddcc1bd2c6dbaddadef138
coduhee <vdyjb@ewha.ac.kr>
2026-01-07 16:57:03 +0900

    feat: 과제2 풀이
    햄드네요

--- juhee/main.swift
+++ juhee/main.swift
@@ -19,3 +19,38 @@ print(sum) // 두 수의 합은 30 입니다. 출력
 func calculate(a: Int, b: Int, closure: (Int, Int) -> String) -> Void {
     closure(a, b)
 }
+
+
+
+// 과제 2 풀이
+
+let numbers = [1, 2, 3, 4, 5]
+
+var result = [String]()
+
+result = numbers.map { String($0) }
+print(result)
+
+
+let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+var result2 = numbers2.filter { $0 % 2 == 0}
+var result3 = result2.map { String($0) }
+print(result3)
+
+
+func myMap (a: [Int], operation: (Int) -> String) -> [String] {
+    var result = [String]()
+    for i in a {
+        result.append(String(i))
+    }
+    return result
+}
+
+let result4 = myMap(a: [1, 2, 3, 4, 5]) {
+    String($0)
+}
+
+print(result)
+
+// 과제 3 풀이

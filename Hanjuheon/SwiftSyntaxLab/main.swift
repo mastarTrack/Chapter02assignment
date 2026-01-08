cee97fd8fae69a3ca00048af51f409f0fb08e3ec
hanjuheon <godzx3@gmail.com>
2026-01-08 21:06:38 +0900

    # feat - 문제 3번 반복문코드 범위 연산자로 변경 - 문제 5번 풀이 추가

--- Hanjuheon/SwiftSyntaxLab/main.swift
+++ Hanjuheon/SwiftSyntaxLab/main.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Hanjuheon on 1/6/26.
 //
-
+//
 import Foundation
 
 /*
@@ -153,7 +153,7 @@ let arrStr: [String] = ["가","나","다","라","마"]
 func removeIntArrEvenIndex(_ target: [Int])->[Int] {
     // # 반복문과 의문문을 이용한 문제 해결
     var result: [Int] = []
-    for i in 0...target.count-1{
+    for i in 0..<target.count{
         if i%2==1{
             result.append(target[i])
         }
@@ -291,5 +291,95 @@ for intro in introducible{
         robots.knead()
     default:
         print("특수 행동이 없습니다.")
+    }
+}
+
+
+
+print("\n\n -----------------------------------------")
+print("\n# 필수문제 풀이 05\n")
+/*
+ 우리는 간단한 **택배 도착 예측 시스템**을 만들고 있다고 가정합니다.
+
+ 사용자에게 예상 도착일을 알려주려 하지만, 다음과 같은 여러 상황에서 문제가 발생할 수 있습니다:
+
+ - 주소가 잘못된 경우
+ - 배송이 아직 시작되지 않은 경우
+ - 시스템 서버 에러로 예측이 불가능한 경우
+ - [ ]  배송 상태를 표현하는 DeliveryStatus 열거형을 구현하고, 아래 3가지 상태를 포함하도록 합니다.
+     - notStarted
+     - inTransit(daysRemaining: Int)
+     - error
+ - [ ]  사용자 정의 에러 타입 DeliveryError를 Error 프로토콜을 따르도록 정의합니다.
+     - invalidAddress
+     - notStarted
+     - systemError(reason: String)
+ - [ ]  아래 시그니처를 가진 throwing function 을 구현해봅니다.
+     
+     ```swift
+     func predictDeliveryDay(for address: String, status: DeliveryStatus) throws -> String
+     ```
+     
+     - 주소가 빈 문자열이면 DeliveryError.invalidAddress를 던져야 합니다.
+     - 배송이 아직 시작되지 않은 경우 DeliveryError.notStarted를 던져야 합니다.
+     - 시스템 에러 상태면 DeliveryError.systemError(reason:)을 던져야 합니다.
+     - 나머지 경우에는 "배송까지 X일 남았습니다." 형태의 문자열을 반환합니다.
+ - [ ]  위 함수를 do-catch 로 호출하고, 각 에러 상황에 따라 사용자에게 다른 메시지를 출력하세요.
+ */
+
+
+// DeliveryStatus 열거형 생성
+enum DeliveryStatus{
+    case notStarted
+    case inTransit(daysRemaining: Int)
+    case error
+}
+
+// Error 열거형 생성
+enum DeliveryError: Error{
+    case invalidAddress
+    case notStarted
+    case systemError(reason: String)
+    
+    // guard를 이용하여 상태 확인
+    func predictDeliveryDay(for address: String, status: DeliveryStatus) throws -> String{
+        guard !address.isEmpty else{
+            throw DeliveryError.invalidAddress
+        }
+        guard case .notStarted = status else {
+            throw DeliveryError.notStarted
+        }
+        guard case .error = status else {
+            throw DeliveryError.systemError(reason: "시스템 에러가 발생하였습니다.")
+        }
+        
+        guard case .inTransit(let daysRemaining) = status else {
+            return "배송일에 문제가 있습니다."
+        }
+
+        return "배송까지\(daysRemaining)일 남았습니다."
+    }
+    
+    // Do-Catch 문으로 변환
+    func predictDeliveryDayAnDoCatch(for address: String, status: DeliveryStatus) throws -> String{
+        do{
+            if address.isEmpty{
+                throw DeliveryError.invalidAddress
+            }
+            switch status{
+            case .notStarted:
+                throw DeliveryError.notStarted
+            case .error:
+                throw DeliveryError.systemError(reason: "시스템 에러가 발생하였습니다.")
+            case .inTransit(let daysRemaining):
+                return "배송까지\(daysRemaining)일 남았습니다."
+            }
+        }catch DeliveryError.invalidAddress{
+            return "빈 주소값이 입력되었습니다. 확인 부탁드립니다."
+        }catch DeliveryError.notStarted{
+            return "아직 상품이 배송되지 않았습니다."
+        }catch DeliveryError.systemError(let errorText){
+            return errorText
+        }
     }
 }

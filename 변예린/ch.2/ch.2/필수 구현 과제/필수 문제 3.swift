053f57f517b4a2d0852f9c1bad41e81cd5c7a92b
Bambu <byrr113@gmail.com>
2026-01-12 19:23:13 +0900

    Chore: 필수 문제 3 수정 - filter 고차함수 내 조건 수정 / d 함수 제네릭 타입 수정

--- 변예린/ch.2/ch.2/필수 구현 과제/필수 문제 3.swift
+++ 변예린/ch.2/ch.2/필수 구현 과제/필수 문제 3.swift
@@ -11,28 +11,29 @@ struct Required3 {
     // Int 배열 짝수번째 요소 제거하여 반환
     func a (_ arr: [Int]) -> [Int] {
         return arr.enumerated()
-            .filter { $0.offset % 2 != 0 }
+            .filter { !$0.offset.isMultiple(of: 2) }
             .map { $0.element }
     }
     
     // String 배열 짝수번째 요소 제거하여 반환
     func b (_ arr: [String]) -> [String] {
         return arr.enumerated()
-            .filter { $0.offset % 2 != 0 }
+            .filter { !$0.offset.isMultiple(of: 2) }
             .map { $0.element }
     }
     
     // 두 함수 a, b를 대체하는 함수
     func c<T> (_ arr: [T]) -> [T] {
         return arr.enumerated()
-            .filter { $0.offset % 2 != 0 }
+            .filter { !$0.offset.isMultiple(of: 2) }
             .map { $0.element }
     }
     
     // Numeric 프로토콜을 준수하는 타입을 파라미터로 받는 함수
-    func d<D: Numeric> (_ arr: [D]) -> [D] {
+    // - 새로운 타입으로 선언할 필요가 없어 T 타입 재사용
+    func d<T: Numeric> (_ arr: [T]) -> [T] {
         return arr.enumerated()
-            .filter { $0.offset % 2 != 0 }
+            .filter { !$0.offset.isMultiple(of: 2) }
             .map { $0.element }
     }    
 }
